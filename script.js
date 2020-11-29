c80465191755b23991e8dbcc5619092b6c26a418
Alex Curran <alexcurran.va@gmail.com>
2020-11-29 09:13:37 -0600

    Using ternary check for offScreenCanvas

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 {
-    const canvas = document.getElementById("canvas");
+    const canvas = HTMLCanvasElement.prototype.transferControlToOffscreen ? document.getElementById("canvas").transferControlToOffscreen() : document.getElementById("canvas");
+    console.log(canvas);
     /** @type {CanvasRenderingContext2D} */
     const ctx = canvas.getContext("2d", { alpha: true });
 
