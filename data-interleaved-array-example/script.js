bdb9ddb5e983479f0333436e773fe4052189914d
Alex Curran <alexcurran.va@gmail.com>
2025-05-21 10:17:49 -0500

    Single interleaved typed array for best CPU cache performance example included

---
+++ data-interleaved-array-example/script.js
@@ -0,0 +1,83 @@
+import { getRandomInt, getRandomFloat, resizeCanvas } from "../modules/utils.js";
+
+const canvas = /** @type {HTMLCanvasElement | null} */ (document.getElementById("canvas"));
+
+if (!canvas) {
+    throw new Error("Could not find canvas element in the DOM.");
+}
+
+const ctx = canvas.getContext("2d", { alpha: true });
+
+// Cache width and height after init runs, to use during clearRect() and update() in animation loop
+let canvasDimensionsWidth;
+let canvasDimensionsHeight;
+
+const FLAKE_DATA_CHUNK_SIZE = 5; // x, y, dx, dy, r -> chunk within interleaved array
+let flakeCount;
+let flakeData;
+
+function init() {
+    const [canvasWidth, canvasHeight] = resizeCanvas(canvas);
+    canvasDimensionsWidth = canvasWidth;
+    canvasDimensionsHeight = canvasHeight;
+    
+    ctx.fillStyle = "#fff";
+    ctx.globalAlpha = 0.7;
+
+    flakeCount = Math.floor(canvasDimensionsWidth / 3);
+    flakeData = new Float32Array(flakeCount * FLAKE_DATA_CHUNK_SIZE);
+
+    for (let i = 0; i < flakeCount; i++) {
+        // initial creation of dimensions for each snowflake
+        resetFlake(i);
+    }
+}
+
+/**
+ * @param {number} i 
+ * @returns {void}
+ */
+function resetFlake(i) {
+    const base = i * FLAKE_DATA_CHUNK_SIZE;
+    flakeData[base + 0] = getRandomInt(0, canvasDimensionsWidth); // x
+    flakeData[base + 1] = getRandomInt(0, -canvasDimensionsHeight); // y
+    flakeData[base + 2] = getRandomFloat(-2, 2); // dx
+    flakeData[base + 3] = getRandomFloat(2, 5); // dy
+    flakeData[base + 4] = getRandomInt(1, 4); // radius
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvasDimensionsWidth, canvasDimensionsHeight);
+    ctx.beginPath();
+
+    for (let i = 0; i < flakeCount; i++) {
+        // for each snowflake...
+        const currentFlakeBase = i * FLAKE_DATA_CHUNK_SIZE;
+        // update x position
+        // x += dx
+        flakeData[currentFlakeBase + 0] += flakeData[currentFlakeBase + 2];
+        // update y position
+        // y += dy
+        flakeData[currentFlakeBase + 1] += flakeData[currentFlakeBase + 3];
+        const radius = flakeData[currentFlakeBase + 4];
+
+        // if the snowflake reaches the bottom of the screen,
+        // reset to above the top
+        if (flakeData[currentFlakeBase + 1] + radius > canvasDimensionsHeight) {
+            resetFlake(i);
+        }
+
+        // ctx.moveTo(x, y)
+        ctx.moveTo(flakeData[currentFlakeBase + 0], flakeData[currentFlakeBase + 1]);
+        // ctx.arc(x, y, radius, 0, Math.PI * 2, true)
+        ctx.arc(flakeData[currentFlakeBase + 0], flakeData[currentFlakeBase + 1], flakeData[currentFlakeBase + 4], 0, Math.PI * 2, true);
+    }
+
+    // Call ctx.fill() only after batch tracing all flakes prior (better performance)
+    ctx.fill();
+}
+
+init();
+animate();
+window.onresize = init;
